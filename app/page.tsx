f084bff22d3079cb305354ed0e63f0f9c7347315
wilson espina <wilson.espina@gmail.com>
2023-05-27 13:54:52 +1200

    feat: adds more inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,33 @@
+'use client';
+
 import styles from './page.module.scss';
 
 export default async function Index() {
+
+  const handleSubmit = (event : React.FormEvent<HTMLFormElement>) : void => {
+    event.preventDefault();
+    console.log('handleSubmit>>>>')
+  }
+
   return (
-    <div>
+    <div className='page'>
       <h1>Bedtime Story generator</h1>
       <p>Welcome to the story generator, fill in the details below to generate a bedtime story</p>
 
 
-    <form action="">
-      <label htmlFor="name">Child&apos;s name</label>
-      <input type="text" name="name" placeholder="Enter your child's name" />
+      <form action="" onSubmit={handleSubmit} className='form'>
+        <label htmlFor="name">Child&apos;s name</label>
+        <input type="text" name="name" placeholder="Enter your child's name" />
 
 
-      <button>Create Story</button>
+        <label htmlFor="minutes">Number of minutes (5-30):</label>
+        <input type="number" id="minutes" name="minutes" min="5" max="30" step={5}></input>
 
-    </form>
+
+        {/* <button>Create Story</button> */}
+        <input type="submit" value="Create Story" />
+
+      </form>
     </div>
   );
 }
