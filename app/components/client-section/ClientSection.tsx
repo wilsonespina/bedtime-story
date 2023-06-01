20746da4787037e3978e97b030a08ba7fb88a0a3
wilson espina <wilson.espina@gmail.com>
2023-06-01 20:28:03 +1200

    feat: all options working and generating response

--- app/components/client-section/ClientSection.tsx
+++ app/components/client-section/ClientSection.tsx
@@ -5,20 +5,21 @@
 import { useState } from "react";
 
 export default function ClientSection() {
-  const [data, setData] = useState({
-    name: "",
-    minutes: 5,
-    age: 2,
-    details: {
-      storyElements: ['']
-    }
-  }); // TODO - add dynamic data structure
+  const [name, setName] = useState('');
+  const [minutes, setMinutes] = useState<number | string>(5);
+  const [storyDetails, setStoryDetails] = useState([]);
+  const [moral, setMoral] = useState('');
+
+
   const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState("");
   const [response, setResponse] = useState<string>("");
 
-  const prompt = `Q: ${input} Generate a response with less than 3000 characters.`;
+  const detailsSting = (details: [string]) => details.join(', ') ?? '';
 
+  const prompt = `Q:
+    Can you make up a bedtime story for a 2-year old called ${name}. Can it include: ${detailsSting(storyDetails)}? Can the moral of the story be ${moral}? Can the story last for ${minutes} minutes?
+    Generate a response with less than 3000 characters.`;
+  
   const generateResponse = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setResponse("");
@@ -57,10 +58,12 @@ export default function ClientSection() {
     setLoading(false);
   };
 
+
+
   return (
     <div className="w-full max-w-xl">
       <form>
-        <textarea
+        {/* <textarea
           value={input}
           onChange={(e) => setInput(e.target.value)}
           rows={4}
@@ -68,16 +71,16 @@ export default function ClientSection() {
           className="focus:ring-neu w-full rounded-md border border-neutral-400
           p-4 text-neutral-900 shadow-sm placeholder:text-neutral-400 focus:border-neutral-900"
           placeholder={"e.g. What is React?"}
-        />
+        /> */}
 
         <div className="focus:ring-neu w-full flex">
           <label htmlFor="name">Child&apos;s name:</label>
-          <input type="text" name="name" placeholder="Enter your child's name" />
+          <input type="text" name="name" placeholder="Enter your child's name" onChange={e => setName(e.target.value)}/>
         </div>
 
         <div className="focus:ring-neu w-full flex">
           <label htmlFor="minutes">Number of minutes (5-30):</label>
-          <input type="number" id="minutes" name="minutes" min="5" max="30" step={5} defaultValue={5}></input>
+          <input type="number" id="minutes" name="minutes" min="5" max="30" step={5} defaultValue={5} onChange={e => setMinutes(Number(e.target.value))}></input>
         </div>
 
         <div className="focus:ring-neu w-full flex">
@@ -85,29 +88,38 @@ export default function ClientSection() {
             <legend>Choose elements for the fairytale:</legend>
 
             <div className="focus:ring-neu w-full flex">
-              <input type="checkbox" id="fairies" name="fairies" />
+              <input type="checkbox" id="fairies" name="elements" value="fairies" onChange={e => setStoryDetails((prev) => [...prev, e.target.value])} />
               <label htmlFor="fairies">Fairies</label>
             </div>
 
-            <div className="focus:ring-neu w-full flex">
-              <input type="checkbox" id="unicorn" name="unicorn" />
+            {/* <div className="focus:ring-neu w-full flex">
+              <input type="checkbox" id="unicorn" name="elements" value="unicorn" />
               <label htmlFor="unicorn">Unicorn</label>
             </div>
 
             <div className="focus:ring-neu w-full flex">
-              <input type="checkbox" id="bee" name="bee" />
+              <input type="checkbox" id="bee" name="elements" value="bee" />
               <label htmlFor="bee">Bee</label>
             </div>
 
             <div className="focus:ring-neu w-full flex">
-              <input type="checkbox" id="mermaid" name="mermaid" />
+              <input type="checkbox" id="mermaid" name="elements" value="mermaid" />
               <label htmlFor="mermaid">Mermaid</label>
-            </div>
+            </div> */}
 
           </fieldset>
         </div>
 
-        
+        <div className="focus:ring-neu w-full flex">
+          <label htmlFor="moral select">Choose a moral:</label>
+
+          <select id="moral select" onChange={e => setMoral(e.target.value)}>
+              <option value="">--Please choose an option--</option>
+              <option value="Always tell the truth">Always tell the truth</option>
+              <option value="Always try your hardest">Always try your hardest</option>
+          </select>
+        </div>
+
       </form>
 
       {!loading ? (
